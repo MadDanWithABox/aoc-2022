5fb56569104ed748e61519df2195e6d5d95c2b5b
MadDanWithABox <valhallaflash@gmail.com>
2022-12-05 12:57:52 +0000

    feat: fix code, and get answer

--- 1/main.py
+++ 1/main.py
@@ -2,21 +2,33 @@ lists=[]
 
 with open("input.txt") as f:
     lines=f.readlines()
+    all_elves=[]
+    current_elf=[]
+    for line in lines:
+        curr=line.strip("\n\r")
+        if curr != "":
+            val = int(curr)
+            current_elf.append(val)
+        else:
+            all_elves.append(current_elf)
+            current_elf=[]
 
-dummy_elves = [[1,2,3], [4,5,6], [7,8],[1000]]
+print(all_elves)
+
+dummy_elves = [[1,2,3],[4,5,6],[7,8],[1000], [900,10000, -29]]
+
 
 def calc_total(snacks):
     total = 0
     for calories in snacks:
-        total += snacks
+        total = total+calories
     return total
 
-calorie_laden_elf_index = None
-most_calories = 0
-
 def find_fattest_elf(elves):
+    calorie_laden_elf_index = None
+    most_calories = 0
     for i, elf in enumerate(elves):
-        calories = calc_total(snacks)
+        calories = calc_total(elf)
         if calories > most_calories:
             calorie_laden_elf_index = i
             most_calories = calories
@@ -25,5 +37,5 @@ def find_fattest_elf(elves):
     
     return solution 
 
-ans = find_fattest_elf(dummy_elves)
+ans = find_fattest_elf(all_elves)
 print(ans)
